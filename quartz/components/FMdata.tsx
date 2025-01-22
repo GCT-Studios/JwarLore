0845f1dfc5ecbce623e650db9dec2ad0d7c34873
Fleafa <Fleafa@users.noreply.github.com>
2025-01-22 10:36:15 +0000

    Update FMdata.tsx

--- quartz/components/FMdata.tsx
+++ quartz/components/FMdata.tsx
@@ -13,8 +13,8 @@ export default (() => {
 		const FMauthor = fileData.frontmatter?.author
 		var authorStr
 
-		if (FMsource != null){ sourceStr = "<strong>Source: </strong>"+{FMsource} }
-		if (FMauthor != null){ authorStr = "<br /><strong>Author: </strong>"+{FMauthor} }
+		if (FMsource != null){ sourceStr = <strong>Source: </strong>{FMsource} }
+		if (FMauthor != null){ authorStr = <br /><strong>Author: </strong>{FMauthor} }
 
 		return <p class={classNames(displayClass, "frontmatter")}>{sourceStr}{authorStr}</p>
 	  } else {
